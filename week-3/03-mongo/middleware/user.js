5b70cc23216aa4441d3aedc59f4efa2ab7088918
Rohit kumar barada <rohitbarada@gmail.com>
2024-03-24 01:28:25 +0530

    working on user

--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,4 +1,4 @@
-import { User } from "../db/index";
+const User = require("../db/index") ;
 async function userMiddleware(req, res, next) {
   // Implement user auth logic
   // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,15 +1,34 @@
-const { Router } = require("express");
-const router = Router();
+const Express = require("express");
+const router = Express();
+const { User, Course } = require("../db");
 const userMiddleware = require("../middleware/user");
 
 // User Routes
 router.post('/signup',(req, res) => {
     // Implement user signup logic
+    const email = req.body.email;
+  const password = req.body.password;
+  const emailResponse = emailSchema.safeParse(email);
+  const passResponse = passwordSchema.safeParse(password);
+  if (!emailResponse.success || !passResponse.success) {
+    res.status(403).send("invalid email or password");
+  } else {
+    User.create({
+      email,
+      password,
+    });
+    res.status(403).json("User created successfully");
+  }
 });
 
 router.get('/courses',(req, res) => {
     // Implement listing all courses logic
-});
+
+    Course.find({}).then((response)=>{
+        res.json({response})
+      })
+    
+    });
 
 router.post('/courses/:courseId',userMiddleware, (req, res) => {
     // Implement course purchase logic
@@ -18,5 +37,5 @@ router.post('/courses/:courseId',userMiddleware, (req, res) => {
 router.get('/purchasedCourses', userMiddleware, (req, res) => {
     // Implement fetching purchased courses logic
 });
-
+router.listen(3000)
 module.exports = router
